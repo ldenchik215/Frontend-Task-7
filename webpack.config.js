f3a2e10988a181b990ce270bafcd2c1a6e95a8cb
Denis <ldenchik215@gmail.com>
2024-11-10 13:36:13 +0700

    config done

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CopyWebpackPlugin  = require("copy-webpack-plugin")
 
 module.exports = {
 	mode: 'development',
@@ -8,13 +11,58 @@ module.exports = {
 	output: {
 		filename: 'index.js',
 		path: path.resolve(__dirname, 'dist'),
+		clean: true,
 	},
-	modules: {
+	plugins: [
+		new HtmlWebpackPlugin({
+      title: 'CPS',
+      template: './src/index.html',
+      inject: true,
+    }),
+		new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+		new CopyWebpackPlugin({
+			patterns: [
+      {
+        from: './src/img',
+        to: 'img',
+      },
+    ]})
+	],
+	module: {
 		rules: [
 			{
 			test: /\.scss$/i,
-			use: ['style-loader', 'css-loader', 'sass-loader'],
+			use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
 			},
+			{
+        test: /\.(svg|png|jpg|jpeg|webp)$/,
+        use: [
+          {
+            loader: 'file-loader?name=./static/[name].[ext]'
+          },
+        ]
+      },
+			{
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader?name=./fonts/[name].[ext]'
+          },
+        ]
+      },
+			{
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      },
 		]
 	},
 	devServer: {
@@ -24,5 +72,5 @@ module.exports = {
 		static: {
 			directory: path.join(__dirname, 'dist')
 		}
-	}
+	},
 }
