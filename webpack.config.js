7f9715809f287bd402d6f93114dc9f4f656d9e98
Denis <ldenchik215@gmail.com>
2024-11-11 22:50:07 +0700

    add html

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,99 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const CopyWebpackPlugin  = require("copy-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-	mode: 'development',
-	entry: {
-		filename: path.resolve(__dirname, 'src/index.js')
-	},
-	output: {
-		filename: 'index.js',
-		path: path.resolve(__dirname, 'dist'),
-		clean: true,
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
+  mode: 'development',
+  entry: {
+    filename: path.resolve(__dirname, 'src/index.js')
+  },
+  output: {
+    filename: 'index.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
       title: 'CPS',
-      template: './src/index.html',
-      inject: true,
+      template: path.resolve(__dirname, './src/index.html'),
+      inject: true
     }),
-		new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-		new CopyWebpackPlugin({
-			patterns: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css'
+    })
+  ],
+  module: {
+    rules: [
       {
-        from: './src/img',
-        to: 'img',
+        test: /\.html$/i,
+        loader: 'html-loader'
       },
-    ]})
-	],
-	module: {
-		rules: [
-			{
-			test: /\.scss$/i,
-			use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
-			},
-			{
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
         test: /\.(svg|png|jpg|jpeg|webp)$/,
         use: [
           {
-            loader: 'file-loader?name=./static/[name].[ext]'
-          },
-        ]
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ],
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[hash][ext][query]'
+        }
       },
-			{
-        test: /\.(eot|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader?name=./fonts/[name].[ext]'
-          },
-        ]
+      {
+        test: /\.(eot|ttf|woff|woff2)$/i,
+        type: 'asset/resource',
+        generator: { filename: 'fonts/[name][ext]' }
       },
-			{
+      {
         test: /\.js$/,
-        include: path.resolve(__dirname, 'src/js'),
+        include: path.resolve(__dirname, './src/js'),
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ['@babel/preset-env']
           }
         }
-      },
-		]
-	},
-	devServer: {
-		port: 9000,
-		compress: true,
-		hot: true,
-		static: {
-			directory: path.join(__dirname, 'dist')
-		}
-	},
+      }
+    ]
+  },
+  devServer: {
+    port: 9000,
+    open: true,
+    compress: true,
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'dist')
+    }
+  }
 }
